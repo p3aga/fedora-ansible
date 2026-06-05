d1474db40e35218bcd53872a6f4d3280836f4e72
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-06-04 21:06:05 -0300

    chore: melhora logs de scripts

--- scripts/change_fingerprint.sh
+++ scripts/change_fingerprint.sh
@@ -3,7 +3,9 @@
 sudo -v
 
 if authselect current | grep -q with-fingerprint; then
+  echo "Desativando sudo com digital"
   sudo authselect disable-feature with-fingerprint
 else
+  echo "Ativando sudo com digital"
   sudo authselect enable-feature with-fingerprint
 fi
